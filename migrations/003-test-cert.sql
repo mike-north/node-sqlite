6c058cab952f07a08fda0df41741a4bbe0ad7528
Michael North <michael.l.north@gmail.com>
2017-11-21 18:20:05 -1000

    temp release

--- migrations/003-test-cert.sql
+++
@@ -1,9 +0,0 @@
--- Up
-CREATE TABLE whatever ( certificate TEXT );
-INSERT INTO whatever ( certificate ) VALUES (
-  '-----BEGIN CERTIFICATE-----
-some contents
------END CERTIFICATE-----');
-
--- Down
-DROP TABLE whatever;
