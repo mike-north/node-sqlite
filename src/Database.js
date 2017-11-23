ef24eb399078d81bf67917d955bde96f597edb47
Michael North <michael.l.north@gmail.com>
2017-11-22 20:51:42 -1000

    Expose sqlite3 debugging hooks

--- src/Database.js
+++ src/Database.js
@@ -11,17 +11,17 @@ import fs from 'fs';
 import path from 'path';
 import Statement from './Statement';
 import prepareParams from './utils';
+import sqlite3 from 'sqlite3';
 
 class Database {
-
   /**
    * Initializes a new instance of the database client.
-   * @param driver An instance of SQLite3 driver library.
-   * @param promiseLibrary ES6 Promise library to use.
+   * @param {sqlite3.Database} driver An instance of SQLite3 driver library.
+   * @param {{Promise: PromiseConstructor}} promiseLibrary ES6 Promise library to use.
      */
-  constructor(driver, { Promise }) {
+  constructor(driver, promiseLibrary) {
     this.driver = driver;
-    this.Promise = Promise;
+    this.Promise = promiseLibrary.Promise;
   }
 
   /**
@@ -37,6 +37,16 @@ class Database {
         }
       });
     });
+  }
+
+  /**
+   * Register listeners for Sqlite3 events
+   *
+   * @param {'trace'|'profile'|'error'|'open'|'close'} eventName
+   * @param {() => void} listener trigger listener function
+   */
+  on(eventName, listener) {
+    this.driver.on(eventName, listener);
   }
 
   run(sql) {
